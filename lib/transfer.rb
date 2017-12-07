ee3cf5c66bdd0ecc3b95687ffdc2be2f67be2e4d
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-07 15:15:26 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
 
     if sender.valid? == false
       @status = 'rejected'
-      return "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
     end
   end
 
