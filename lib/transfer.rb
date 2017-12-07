03b934173882f6558aad4018d2585eb91859793d
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-07 14:44:12 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,23 +13,23 @@ class Transfer
   def valid?
     sender.valid? && receiver.valid?
   end
-
-  def execute_transaction
-    if status == 'pending' && sender.valid?
-      sender.balance -= amount
-      receiver.deposit(amount)
-      status = 'complete'
-    elsif sender.valid? == false
-      return "Transaction rejected. Please check your account balance."
-    end
-  end
-
-  def reverse_transfer
-    if status == 'complete'
-      receiver.balance -= amount
-      sender.deposit(amount)
-      status = 'reversed'
-    end
-  end
+  # 
+  # def execute_transaction
+  #   if status == 'pending' && sender.valid?
+  #     sender.balance -= amount
+  #     receiver.deposit(amount)
+  #     status = 'complete'
+  #   elsif sender.valid? == false
+  #     return "Transaction rejected. Please check your account balance."
+  #   end
+  # end
+  #
+  # def reverse_transfer
+  #   if status == 'complete'
+  #     receiver.balance -= amount
+  #     sender.deposit(amount)
+  #     status = 'reversed'
+  #   end
+  # end
 
 end
