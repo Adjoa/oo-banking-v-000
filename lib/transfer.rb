ae4bced17ebd615f36269a82a7f126ebb0fbb5c2
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-07 14:54:33 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,12 +17,12 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.valid? == false
-      return "Transaction rejected. Please check your account balance."
-    elsif @status == 'pending' && @sender.valid?
-      @sender.balance -= @amount
-      @receiver.deposit(@amount)
+    if @status == 'pending' && sender.valid?
+      sender.balance -= @amount
+      receiver.deposit(@amount)
       @status = 'complete'
+    elsif sender.valid? == false
+      return "Transaction rejected. Please check your account balance."
     end
   end
   #
