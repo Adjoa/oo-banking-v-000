f893ae0fb7c81ca203e97ab7e3c393186d3b2e9f
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-07 15:05:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,19 +23,19 @@ class Transfer
       receiver.deposit(@amount)
       @status = 'complete'
     end
-    binding.pry
+    # binding.pry
     if sender.valid? == false
       @status = 'rejected'
       return "Transaction rejected. Please check your account balance."
     end
   end
-  #
-  # def reverse_transfer
-  #   if status == 'complete'
-  #     receiver.balance -= amount
-  #     sender.deposit(amount)
-  #     status = 'reversed'
-  #   end
-  # end
+
+  def reverse_transfer
+    if status == 'complete'
+      receiver.balance -= amount
+      sender.deposit(amount)
+      status = 'reversed'
+    end
+  end
 
 end
